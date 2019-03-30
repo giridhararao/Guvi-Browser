64386a79aef2470f546d9776cfa237afa9948999
M.Giridhara Rao <www.giridhararao@gmail.com>
2019-03-30 11:35:00 +0530

    Create CloseAppBox.java

---
+++ application/controller/CloseAppBox.java
@@ -0,0 +1,49 @@
+package main.java.com.goxr3plus.javafxwebbrowser.application.controllers;
+
+import java.io.IOException;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.StackPane;
+import main.java.com.goxr3plus.javafxwebbrowser.application.Main;
+import main.java.com.goxr3plus.javafxwebbrowser.tools.InfoTool;
+
+public class CloseAppBox extends StackPane {
+	
+	
+	@FXML
+	private JFXButton minimize;
+	
+	@FXML
+	private JFXButton maxOrNormalize;
+	
+	@FXML
+	private JFXButton exitApplication;
+  
+	public CloseAppBox() {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(InfoTool.FXMLS + "CloseAppBox.fxml"));
+		loader.setController(this);
+		loader.setRoot(this);
+		
+		try {
+			loader.load();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+	
+	@FXML
+	private void initialize() {
+		
+		minimize.setOnAction(ac -> Main.window.setIconified(true));
+
+		maxOrNormalize.setOnAction(ac -> Main.borderlessScene.maximizeStage());
+
+		exitApplication.setOnAction(ac -> System.exit(0));
+		
+	}
+	
+}
